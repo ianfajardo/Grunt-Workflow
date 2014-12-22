163c12cdffea00a7a7a041c346883625cf40e200
Ian Fajardo <ian.fajardo@blackbaud.com>
2014-12-22 15:00:58 -0500

    Grunt Express

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,17 +13,20 @@ module.exports = function(grunt) {
 		},
 		watch: {
 		  all: {
-			// Replace with whatever file you want to trigger the update from
-			// Either as a String for a single entry 
-			// or an Array of String for multiple entries
-			// You can use globing patterns like `css/**/*.css`
-			// See https://github.com/gruntjs/grunt-contrib-watch#files
 			files: ['index.html', 'style.css'],
 			options: {
 			  livereload: true
 			}
 		  }
 		},
+		notify_hooks: {
+		    options: {
+		      enabled: true,
+		      max_jshint_notifications: 5,
+		      success: true,
+		      duration: 3
+		    }
+		  },
 		dom_munger: {
 			your_target: {
 				options: {
@@ -39,7 +42,8 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-dom-munger');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-express');
-	
-	grunt.registerTask('default', ['dom_munger']);
-	grunt.registerTask('live', ['express', 'watch']);
+	grunt.loadNpmTasks('grunt-notify');
+	grunt.task.run('notify_hooks'); 
+	grunt.registerTask('default', ['notify_hooks','dom_munger']);
+	grunt.registerTask('live', ['notify_hooks', 'express', 'watch']);
 };
