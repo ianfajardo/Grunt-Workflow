b9ebf0038c8bc0a6d9404950eaca2d8c3fac6b47
Ian Fajardo <ian.fajardo@blackbaud.com>
2014-12-30 14:51:11 -0500

    Updated livereload script in HTML

--- scripts/init.js
+++ scripts/init.js
@@ -1,3 +1,5 @@
 document.getElementById("seeMe").innerHTML = "<h1>Test</h1>";
 
-document.getElementById("this").innerHTML = "<h1>Test2</h1>"
+document.getElementById("this").innerHTML = "<h1>Test2</h1>";
+
+
